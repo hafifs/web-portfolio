fbcbb57b5ea22727981862cbedc31c313b410bd5
hafifs <161149626+hafifs@users.noreply.github.com>
2024-12-11 02:49:47 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="dark:bg-black">
+        <h1 className="text-5xl dark:text-white">hello world</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
